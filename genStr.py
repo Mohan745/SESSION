b1f21ffeaaeea97d20b291735aa9b639ad5a5be3
Mohan745 <89540820+Mohan745@users.noreply.github.com>
2021-10-04 18:45:06 +0530

    Update genStr.py

--- genStr.py
+++ genStr.py
@@ -154,7 +154,7 @@ Must Join Channel for Bot Updates !!
     reply_markup = InlineKeyboardMarkup(
         [
             [
-                InlineKeyboardButton('Developer', url='HTTPS://T.ME/HACKER_ZD)
+                InlineKeyboardButton('Developer', url='HTTPS://T.ME/HACKER_ZD,)
             ],
             [
                 InlineKeyboardButton('Contributor', url='@Alone_loverboy'),
